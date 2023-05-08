28e07d4063402e15dcf20c9f8eaeaabc73303003
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2023-05-08 19:05:51 +0300

    change

--- src/KeyBoard.java
+++ src/KeyBoard.java
@@ -5,6 +5,7 @@ public class KeyBoard implements KeyListener {
     private Game game;
     Cannon cannon;
     private static boolean startShooting;
+    private static boolean isHide;
     public KeyBoard(Game game,Cannon cannon){
        this.game =game;
        this.cannon = cannon;
@@ -18,9 +19,9 @@ public class KeyBoard implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
      switch (e.getKeyCode()){
-         case KeyEvent.VK_SPACE -> {startShooting=true;this.game.createShot();}
-         case KeyEvent.VK_LEFT -> {this.cannon.moveCannon(-24);this.cannon.setLeft(true);}
-         case KeyEvent.VK_RIGHT -> {this.cannon.moveCannon(24);this.cannon.setLeft(false);}
+         case KeyEvent.VK_SPACE -> {startShooting=true;this.game.createShot(); isHide=true;}
+         case KeyEvent.VK_LEFT -> {this.cannon.moveCannon();this.cannon.setLeft(true); isHide=true;}
+         case KeyEvent.VK_RIGHT -> {this.cannon.moveCannon();this.cannon.setLeft(false); isHide=true;}
      }
     }
 
@@ -33,5 +34,10 @@ public class KeyBoard implements KeyListener {
         switch (e.getKeyCode()){
             case KeyEvent.VK_SPACE -> {startShooting=false;}
         }
+    }
+
+    public boolean isHide(){
+        System.out.println("");
+        return isHide;
     }
 }
